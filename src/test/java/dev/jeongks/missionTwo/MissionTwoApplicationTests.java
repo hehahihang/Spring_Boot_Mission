1f840145680d197cd80a0f1adefa89f1084c3979
JeongKwangSu <soo0546@naver.com>
2022-03-10 17:36:31 +0900

    first

---
+++ src/test/java/dev/jeongks/missionTwo/MissionTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jeongks.missionTwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MissionTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
